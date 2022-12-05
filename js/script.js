b851034f351add1e04c39b6bd0d767a5e19076a4
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-05 15:48:27 +0100

    step di lavoro

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log('JS OK');
+
+//Calcolare prezzo del biglietto del treno in base a percorrenza e età del passeggero
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65
+
+
+/* 1. Chiedere all'utente quanti km vuole percorrere
+2. Chiedere all'utente quanti anni ha
+3. Calcolare prezzo totale del viaggio (prezzo finale per utenti 19-64 anni)
+4. Applicare sconti al totale del viaggio: 20% <18 anni, 40% >65 anni (con if/else)
+5. Stampare in pagina */
+
+
+
+
+
+
